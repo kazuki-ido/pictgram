5fff5f9fb5a452b0ffd832b01303046e2cfb0973
kazuki ido <0sn58m19324338p@gmail.com>
2020-05-10 18:05:16 +0000

    コメント機能実装

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -9,6 +9,7 @@ class Topic < ApplicationRecord
   
   mount_uploader :image, ImageUploader
   
+  has_many :comments
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: "user"
 end
